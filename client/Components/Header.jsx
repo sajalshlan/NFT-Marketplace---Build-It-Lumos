bbd31cd05ac4d63cabea1b1dce2adb83f177da6b
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2022-11-06 12:34:55 +0530

    adding all the changes done on 5

---
+++ client/Components/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import NFTCard from './NFTCard';
+import bandar from '../public/bandar.png';
+
+const title = text => {
+  return (
+    <div className="text-white rounded-lg font-semibold bg-gray-600 py-2 px-3 hover:bg-[#232323] cursor-pointer">
+      {text}
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="flex w-[85%] justify-between mx-auto h-screen py-20">
+      <div className="flex relative ">
+        <NFTCard
+          className="z-20"
+          img={bandar}
+          title="Bored Ape"
+          price={5.3432}
+        />
+        <NFTCard
+          className="absolute left-16 rotate-[16deg] z-10"
+          img={bandar}
+          title="Bored Ape"
+          price={5.3432}
+        />
+        {/* <NFTCard className="absolute left-20 rotate-[24deg]" /> */}
+      </div>
+      <div className="w-[35rem]">
+        <h1 className="text-5xl text-[#232323] font-serif font-semibold leading-[3.2rem] ">
+          Discover, <br />
+          <span className="text-gray-600"> Collect and Sell </span> <br /> Extra
+          Ordinary NFTs
+        </h1>
+        <p className="text-lg font-medium text-gray-500 mt-3">
+          Explore the top collection of NFTs in this awesome market place and
+          buy & sell NFTs at the finest price.
+        </p>
+
+        <div className="flex flex-wrap gap-3 mt-10">
+          {title('Ape Collection')}
+          {title('Newlyy')}
+          {title('Avatars')}
+          {title('Music')}
+          {title('Groovy')}
+          {title('Gamified NFTs')}
+          {title('Artworks')}
+          {title('Photography')}
+          {title('Collectibles')}
+        </div>
+
+        <button className="text-white uppercase tracking-wide font-semibold py-3 text-2xl bg-[#232323] w-full rounded-lg  mt-20 border-2 border-gray-800 shadow-2xl hover:bg-gray-800 hover:scale-105 transition-all ease-in-out">
+          Mint Now
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link';
+import { FaWallet } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+
+const Navbar = () => {
+  const router = useRouter();
+
+  const connectWalletPageHandler = () => {
+    router.push('/wallets');
+  };
+
+  return (
+    <nav className="flex justify-around py-5 items-center bg-[#232323]">
+      <Link
+        href="/"
+        className="text-white text-3xl font-semibold cursor-pointer"
+      >
+        assHub
+      </Link>
+      <ul className="flex space-x-10 text-white uppercase text-sm tracking-wider">
+        <li className="hover:scale-105 transition-all ease-in font-semibold">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:scale-105 transition-all ease-in font-semibold">
+          <Link href="/">My Assets</Link>
+        </li>
+        <li className="hover:scale-105 transition-all ease-in font-semibold">
+          <Link href="/forms">Sell Assets</Link>
+        </li>
+      </ul>
+      <button
+        onClick={connectWalletPageHandler}
+        className="flex items-center gap-2 text-black bg-white py-2 px-4 rounded-lg font-semibold uppercase tracking-wide hover:scale-105 transition-all ease-in-out"
+      >
+        Connect Wallet
+        <span>
+          <FaWallet />
+        </span>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
